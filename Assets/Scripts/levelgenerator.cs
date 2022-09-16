04ebc3805f5d41b02af076a471092f24e317d6e9
zy22187 <zy22187@nottingham.edu.cn>
2022-09-16 13:35:25 -0700

    add falling rocks

--- Assets/Scripts/levelgenerator.cs
+++ Assets/Scripts/levelgenerator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class levelgenerator : MonoBehaviour
 {
     public GameObject pipe;
+    public GameObject fallingObj;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +24,20 @@ public class levelgenerator : MonoBehaviour
     {
         float randomNumber = Random.Range(0, 1f);
         if (randomNumber > 0.990f){
-        GameObject ppipe = (GameObject)Instantiate (pipe);
-        ppipe.transform.rotation = Quaternion.identity;
-        ppipe.transform.position = new Vector3(Random.Range(-5, 5f), 1, 36);
-        Rigidbody m_Rigidbody = ppipe.GetComponent<Rigidbody>();
-        m_Rigidbody.velocity = new Vector3(0,0,-15f);
+            GameObject ppipe = (GameObject)Instantiate (pipe);
+            ppipe.transform.rotation = Quaternion.identity;
+            ppipe.transform.position = new Vector3(Random.Range(-5, 5f), 1, 36);
+            Rigidbody m_Rigidbody = ppipe.GetComponent<Rigidbody>();
+            m_Rigidbody.velocity = new Vector3(0,0,-15f);
+        }
+
+        // check if generate a new falling stone
+        if (randomNumber > 0.990f){
+            GameObject rock = (GameObject)Instantiate (fallingObj);
+            rock.transform.rotation = Quaternion.identity;
+            rock.transform.position = new Vector3(Random.Range(-5, 5f), Random.Range(10, 20f), 36);
+            Rigidbody rb_rock = rock.GetComponent<Rigidbody>();
+            rb_rock.velocity = new Vector3(0,0,-15f);
         }
     }
 }

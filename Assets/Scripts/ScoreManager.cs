84b344fc58d37a9d8615d4b85a6ed7a292c3665f
Headwind <danyunwa@usc.edu>
2022-10-07 00:02:18 -0700

    update gameover scene
    shows the died reason

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 using UnityEditor;
 public class ScoreManager : MonoBehaviour {
     public static int sscore = 0;
-	
+
 	public static int biggerCube = 0;
 	public static int smallerCube = 0;
 	public static int shooter = 0;
@@ -13,6 +13,8 @@ public class ScoreManager : MonoBehaviour {
 	public static int invisible = 0;
 	public static bool killedByRock = false;
 	public static bool killedByWater = false;
+	public static bool killedByCeil = false;
+	public static bool killedByBound = false;
 	public static float startTime = Time.time;
 	
 

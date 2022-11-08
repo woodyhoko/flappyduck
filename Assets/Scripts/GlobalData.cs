4a6ee4331c2592422780c841a9634718d45a3f6f
qihanghuUSC <qihanghu@usc.edu>
2022-11-08 15:56:25 -0800

    Clone works with bigger, smaller, move_forward, faster on star update. Clone feature has bugs.

--- Assets/Scripts/GlobalData.cs
+++ Assets/Scripts/GlobalData.cs
@@ -11,6 +11,7 @@ public class GlobalData : MonoBehaviour
     public int shoot_freq = 50;
     public int shoot_timestep = 0;
     public float move_forward_limit = 4.0f;
+    public bool move_forward = false;
     public float move_speed = 0.08f;
     public int update_max_limit = 12;
     public int ate = 0;
@@ -18,9 +19,11 @@ public class GlobalData : MonoBehaviour
     public float plane_width = 10;
     public float world_speed = 1f;
     public float star_size = 0.1f;
+    public Vector3 player_localScale = new Vector3(1f, 1f, 1f);
     public int world = 1;
     public int star_num = 0;
     public int[] cloned_cubes = new int[] { 0, 0 };
+    public List<GameObject> cloned_list = new List<GameObject>();
 
     // dizzness
     public bool dizzy = false;

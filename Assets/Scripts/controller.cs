dcd81bb35f4a78a7388b74d3fa488632a5e467e8
Headwind <danyunwa@usc.edu>
2022-11-23 12:26:45 -0800

    Finished level 2 mid

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -274,6 +274,20 @@ public class controller : MonoBehaviour
             }
         }
 
+        if (level && this_Level_name == "Level_2_mid")
+        {
+            if (timer >= 1350)
+            {
+                ScoreManager.cube_health = GlobalData.Instance.cube_health;
+                ScoreManager.cube_health_org = GlobalData.Instance.hearts.Count;
+                ScoreManager.level11Passed = true;
+                ScoreManager.killedByBound = false;
+                ScoreManager.killedByCeil = false;
+                ScoreManager.killedByWater = false;
+                showResultPage("Level_1_mid Passed", true);
+            }
+        }
+
         if (level && this_Level_name == "Level_3_mid_dy")
         {
             if (timer >= 950)

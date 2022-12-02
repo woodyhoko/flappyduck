e024c0d81a1683e25ea71b6c52e2ef3bbb9578ce
yingjiyan <98205694+yingjiyan@users.noreply.github.com>
2022-12-02 00:39:28 -0800

    water

--- Assets/Scripts/water.cs
+++ Assets/Scripts/water.cs
@@ -20,9 +20,29 @@ public class water : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-       
 
-        
+
+        if (collision.gameObject.tag == "cloned_cube")
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Player" && collision.gameObject.tag != "star")
+        {
+            Debug.Log("---------------water triggger!!------------");
+            GlobalData.Instance.cube_health -= 1;
+            int hp = GlobalData.Instance.cube_health;
+            if (hp > 0)
+                GlobalData.Instance.hearts[hp].SetActive(false);
+            if (hp <= 0f)
+            {
+                ScoreManager.killedByWater = false;
+                ScoreManager.killedByPipe = true;
+                ScoreManager.killedByCeil = false;
+                ScoreManager.killedByBound = false;
+            }
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider collider)
